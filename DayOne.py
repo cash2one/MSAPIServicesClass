723d5b4b29aeb313c607ed8f656a8aaad8b6a185
twof <fabiobean2@gmail.com>
2016-09-27 13:43:39 -0700

    initial commit

---
+++ DayOne.py
@@ -0,0 +1,8 @@
+# This is going to have notes and relevant code from each day
+# This file is for day one
+'''
+These are block comments
+I'm going to use these from here on out because putting a hash at the
+beginning of every line is super annoying
+
+'''
